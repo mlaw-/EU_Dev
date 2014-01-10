d3a41e87aa694a4f68132dcff2816e633d590f04
mlaw- <mlaw@eucmail.com>
2014-01-10 15:31:38 -0500

    OMS API refactoring
    Moved models/sql logic into their own library (located in Base)

--- EU My Account Rest API/EU WebServices/Controllers/OutageController.cs
+++ EU My Account Rest API/EU WebServices/Controllers/OutageController.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using OMS.Models;
+using OMS.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using WebApplication1.Models;
-using WebApplication1.SQL;
 
 namespace WebApplication1.Controllers
 {
@@ -19,7 +19,7 @@ namespace WebApplication1.Controllers
             DateTime tmp2;
             int tmp3;
 
-            return cpApp_OutageManagementSystem.getOutage(
+            return OutageRepository.getOutage(
                 (from == "" || !DateTime.TryParse(from, out tmp)) ? null : (DateTime?)tmp,
                 (to == "" || !DateTime.TryParse(to, out tmp2)) ? null : (DateTime?)tmp2,
                 (openOnly == "1" ? true : false),
@@ -28,19 +28,26 @@ namespace WebApplication1.Controllers
         }
 
         // POST api/outage
-        public void Post([FromBody]string value)
+        [HttpPost]
+        public void Post(string custNbr, string locationID, int serviceTypeID, int problemTypeID,  DateTime dateReceived, int dispatchedID,
+            string notes = null, DateTime? dateDispatched = null, DateTime? dateReconnected = null)
         {
-
+            OutageRepository.createOutage(custNbr, locationID, serviceTypeID, problemTypeID, dateReceived, dispatchedID, notes, dateDispatched, dateReconnected);
         }
 
         // PUT api/outage/5
-        public void Put(int id, [FromBody]string value)
+        [HttpPut]
+        public void Put(int id, string custNbr = null, string locationID = null, string notes = null, int? serviceTypeID = null, int? problemTypeID = null, DateTime? dateReceived = null, int? dispatchedID = null,
+             DateTime? dateDispatched = null, DateTime? dateReconnected = null)
         {
+            OutageRepository.updateOutage(id, custNbr, locationID, notes, serviceTypeID, problemTypeID, dateReceived, dispatchedID, dateDispatched, dateReconnected);
         }
 
         // DELETE api/outage/5
+        [HttpDelete]
         public void Delete(int id)
         {
+            OutageRepository.deleteOutage(id);
         }
     }
 }
